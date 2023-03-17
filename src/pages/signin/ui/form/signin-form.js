0b08cf2b7893868c9848676677a8383a893bc5e9
Anton Trafimovich <antont@compatibl.com>
2023-03-17 17:39:09 +0100

    fix: use correct store for signin form.

--- src/pages/signin/ui/form/signin-form.js
+++ src/pages/signin/ui/form/signin-form.js
@@ -1,15 +1,17 @@
-import { useSignInPageStore } from "../../model";
+import { useAppStore } from "@/app";
+
 import styles from "./signin-form.module.css";
 
 export const SignInForm = () => {
-  const signIn = useSignInPageStore((state) => state.signin);
-  const tokens = useSignInPageStore((state) => state.tokens);
+  const signIn = useAppStore((state) => state.signin);
+  const tokens = useAppStore((state) => state.tokens);
 
   return (
     <>
-      {tokens.status === "error" && <span>{tokens.payload.message}</span>}
+      {tokens.status === "error" && (
+        <span>{tokens.payload.message}</span>
+      )}
       <form
-        action="/api/signin"
         method="post"
         onSubmit={(e) => {
           e.preventDefault();

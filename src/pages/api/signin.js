ebad6d0ca489b795cb729a86f06a88dd8aa45d27
Anton Trafimovich <antont@compatibl.com>
2023-03-17 17:39:55 +0100

    fix: use common getBodyFromResponse in signin request.

--- src/pages/api/signin.js
+++ src/pages/api/signin.js
@@ -1,29 +1,5 @@
+import { getBodyFromResponse } from "@/shared";
 import { request } from "node:http";
-
-const getBodyFromResponse = (res) => {
-  return new Promise((resolve, reject) => {
-    let result = "";
-
-    res
-      .on("data", (chunk) => {
-        result += chunk;
-      })
-      .on("error", (err) => {
-        reject(err);
-      })
-      .on("end", () => {
-        let resultJSON;
-
-        try {
-          resultJSON = JSON.parse(result);
-        } catch (err) {
-          reject(new Error("Error during parsing response body has occured."));
-        }
-
-        resolve(resultJSON);
-      });
-  });
-};
 
 export default function handler(req, res) {
   const signUpRequest = request(

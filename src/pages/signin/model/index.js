757f73650f9a158c7e0a9b6e866dda3413f04221
Anton Trafimovich <antont@compatibl.com>
2023-03-17 17:42:28 +0100

    get rid of signin model.

--- src/pages/signin/model/index.js
+++
@@ -1,8 +0,0 @@
-import { createTokensSlice } from '@/entities';
-import { create } from 'zustand';
-
-export const useSignInPageStore = create((...a) => {
-  return {
-    ...createTokensSlice(...a),
-  };
-});
